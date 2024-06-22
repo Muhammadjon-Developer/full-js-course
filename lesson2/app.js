715f2bb518bad41dab51787411c345b55851fe15
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-06-22 18:45:40 +0300

    2 lesson = variables const, let, var

---
+++ lesson2/app.js
@@ -0,0 +1,26 @@
+// let number = 10;
+// const borderColor = 'red';
+// console.log(number) // 10
+
+// number = 20;
+// console.log(number) result of the number is 20 because we changed the value we can change values of let ✅
+
+// let number = 10;
+// const borderColor = 'red';
+
+// borderColor = 'blue' // this is not avaible to change values of constants = ERROR ❌
+
+// const person = {
+//   name: 'Muhammadjon'   // don't thing about this is an object you can know about in next lessons | The reason why i am using object to explain you that you can change the value of constants and how:
+// }
+
+// person.name = 'Bob';
+// console.log(person);
+
+
+
+// Old version
+
+console.log(clientName) // we will get undifined if you don't know what is "undifined" you can understand it in next lesson there is not logic, if we use let, or const in the place of var we will got an error but we getting undifined so guys i don't recommend you to use this var
+ 
+var clientName = 'John';    // i just recommed you don't use var in this days because it very old, and var works every time
