158fc32641def57720a2e508148e73e550ce7e64
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-06-22 18:59:35 +0300

    lesson 3 use strict we use strcit mode to don't malke an errors in our code if we use old codes of js you can know about in js file

---
+++ lesson 3/app.js
@@ -0,0 +1,6 @@
+// we use use strict to don't make mistakes in our code with writing old code if you write old code and if you write use strict in your file this file shows a error in your code if code is old. If you paste use strict to your 1 function this function will working in strickt mode if you paste the use strict to your code at the top use strict will work with all functions and codes
+
+"use strict";
+
+number = 10;
+console.log(number);
