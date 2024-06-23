4a8fb5b3f71a2a13147ff11657a42e53dade5ad8
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-06-23 21:33:12 +0300

    lesson 6 interpolation

---
+++ lesson 6/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// const channel = prompt("What's your favourite youtuber");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Muhammadjon";
+// const job = "JavaScript Developer";
+// console.log(`User name is ${user}, he is ${job}`);
