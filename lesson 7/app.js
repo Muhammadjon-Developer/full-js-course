600f1b439ee19832dec4d4dbd637e5d9a29cd4f1
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-06-25 10:03:20 +0300

    lesson 7 operators in javascript

---
+++ lesson 7/app.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// console.log("Muhammadjon" + " Olimov"); // to connect 2 strings
+// console.log("Muhammadjon" + 10); // we can also connect string and number
+
+// pretix:
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++); // console: 10 you will see the changes in the 12 
+// console.log(decr--); // console: 10 you will see the changes in the 13
+
+// console.log(incr); // console: 11 = +1
+// console.log(decr); // console: 9 = -1
+
+
+// prefix:
+const incr = 10;
+const decr = 10;
+
+// console.log(++incr); // console: 11 in this prefix way we can get the value 
+// console.log(--decr); // console: 9
+
+
+
+// console.log(5 % 2); // % returns the residual = 1
+// console.log(8 % 3); // % returns the residual = 2
+
+// console.log(5 * 5 == 25); // == - compares the value of numbers
+// console.log(5 * 5 === "25"); // === - compares the value of numbers and data types
+
+// && = and , || = or
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+
+// console.log(isAge && isClose && isChecked); // returns false
+// console.log(isAge || isClose || isChecked); // returns true
+
+// // ! - negation sign
+// const something = true;
+// const something2 = false;
+
+// console.log(!something); // returns false
+// console.log(!something2); // returns true
+
+console.log(2 + 2 * 2 !== "6");
