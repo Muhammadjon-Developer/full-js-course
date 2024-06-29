9774a65fd53bffc3de0fbb835f60e32c23afe8a2
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-06-29 08:42:55 +0300

    writing first js codes and creating function for drawing to canvas

---
+++ Javascript Paint Project/js/app.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("canvas");
+
+let ctx = canvas.getContext("2d"),
+  isDrawing = false;
+  brushWidth = 5;
+
+window.addEventListener("load", () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+})
+
+const startDraw = () => {
+  isDrawing = true;
+  ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+
+};
+
+const drawing = e => {
+  if(!isDrawing) return
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+}
+
+const stopDraw = () => {
+  isDrawing = false;
+}
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", stopDraw);
