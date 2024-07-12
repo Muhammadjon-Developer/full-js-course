cf3358a4c261107783f88103ecf8de2f551b7827
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-12 15:16:43 +0300

    local variable vs global variable

--- Javascript Basics/lesson 11/app.js
+++ Javascript Basics/lesson 11/app.js
@@ -18,9 +18,13 @@
 // calc(45, 56);
 // calc(78, 61);
 
+let age = 15; // global variable
 
 function sayMyName(name, surname) {
+  let age = 21; // local variable
   console.log(`My name is ${name} ${surname}`); // = My name is John Smith
+  console.log(age); // Answer: 21
 }
 
 sayMyName("John", "Smith");
+console.log(age); // Answer: 15, if you calling to age variable in the function Error: age is not defined because it is local variable we can use it only in inside the function
