a8c1d86eaee077cbd009419aa3a8528ec7fed493
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-15 16:02:24 +0300

    2 method and 1 property

---
+++ Javascript Basics/lesson 12/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// 1 - length
+// const user = "Abdulaziz"; // 9
+// const user2 = "Oybek"; // 5
+
+// console.log(user.length); // Returns the number of characters (elements) in a string
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] // 10 elements
+
+// console.log(arr.length);
+
+let person = "Peter";
+// console.log(person[2]); // t, count will start from 0
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(array[5]) // Returns 6, because count will start from 0
+
+
+
+// 2 - toUpperCase()
+
+// REMEMBER: All methods returns as a new string it doesn't change the original string
+
+let studentName = "thomas";
+console.log(studentName.toUpperCase()); // thomas = THOMAS
+
+// 3 - toLowerCase()
+
+let personName = "THOMAS";
+console.log(personName.toLowerCase()); // THOMAS = thomas
