da1cd7394358302a90e09ef2b090da331a755582
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-16 11:35:04 +0300

    slice method

--- Javascript Basics/lesson 12/app.js
+++ Javascript Basics/lesson 12/app.js
@@ -10,10 +10,10 @@
 
 // console.log(arr.length);
 
-let person = "Peter";
-// console.log(person[2]); // t, count will start from 0
+// let person = "Peter";
+// // console.log(person[2]); // t, count will start from 0
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // console.log(array[5]) // Returns 6, because count will start from 0
 
 
@@ -22,10 +22,29 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // REMEMBER: All methods returns as a new string it doesn't change the original string
 
-let studentName = "thomas";
-console.log(studentName.toUpperCase()); // thomas = THOMAS
+// let studentName = "thomas";
+// console.log(studentName.toUpperCase()); // thomas = THOMAS
 
 // 3 - toLowerCase()
 
-let personName = "THOMAS";
-console.log(personName.toLowerCase()); // THOMAS = thomas
+// let personName = "THOMAS";
+// console.log(personName.toLowerCase()); // THOMAS = thomas
+
+
+
+// 4 - indexOf()
+// const greeting = "Hello"; // Count will start from 0
+
+// console.log(greeting.indexOf("o")); // The index of this letter o it's 4
+// console.log(greeting.indexOf("r")); // Returns -1 = false because this letter is not exist in the greeting variable
+// console.log(greeting.indexOf("w")); // Returns -1 = false because this letter is not exist in the greeting variable
+// console.log(greeting.indexOf("p")); // Returns -1 = false because this letter is not exist in the greeting variable
+
+
+// 5 - slice() 
+const sayHelloWorld = "Hello world";
+
+console.log(sayHelloWorld.slice(0, 6)); // Returns Hello
+console.log(sayHelloWorld.slice(6, 11)); // Returns world
+console.log(sayHelloWorld.slice(6)); // Returns world
+console.log(sayHelloWorld.slice(-5, -1)); // Returns worl we don't use this slice with negative numbers every time
