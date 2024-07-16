29ece60d09e97926a29f15c74e12900a979588ad
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-16 22:17:20 +0300

    serial haqida proyekt optimizatsiya uyga vazifani bajaring | a project optimization about series do h/w

---
+++ Javascript Basics/lesson 13/app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+let numberOfSeries;
+
+function startApp() {
+  numberOfSeries = +prompt("Nechta serial ko'rdingiz ?", "");
+
+  while (
+    numberOfSeries == "" ||
+    numberOfSeries == null ||
+    isNaN(numberOfSeries)
+  ) {
+    numberOfSeries = +prompt("Nechta serial ko'rdingiz ?", "");
+  }
+}
+
+startApp();
+
+const seriesDB = {
+  count: numberOfSeries,
+  series: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+function rememberMySeries() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Oxirgi ko'rgan serialingiz ?"),
+      b = prompt("Nechi Baxo berasiz ?");
+
+    if (a != null && b != null && a != "" && b != "") {
+      seriesDB.series[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
+
+rememberMySeries();
+
+function detectLevelSeries() {
+  if (seriesDB.count < 5) {
+    console.log("Kam serial ko'ripsiz");
+  } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+    console.log("Siz classic tomoshabin ekansiz");
+  } else if (seriesDB.count >= 10) {
+    console.log("Siz serialchi zvezda ekansiz");
+  } else {
+    console.log("Error");
+  }
+}
+
+detectLevelSeries();
+
+console.log(seriesDB);
