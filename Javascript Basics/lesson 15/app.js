6d9647782cbee5b8973e572fae351a81a9808cb2
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-07-20 07:00:45 +0300

    obyektlar bilan ishlash yakunlandi | ended lesson working with objects

--- Javascript Basics/lesson 15/app.js
+++ Javascript Basics/lesson 15/app.js
@@ -6,13 +6,39 @@ const theif = {
   colors: {
     hair: "gray",
     style: "curley"
+  },
+  howOut: () => {
+    console.log("Fast with help doors");
   }
 }
 
-// console.log(theif.height); // 1.7
+theif.howOut();
 
-// Metodlar:
+const {hair, style} = theif.colors; // = Destruptizatsiya obyekt katilaridan qiymatlarni olish
 
-// delete theif.jacket; // o'chirish metodi - object dan biror bir narsa o'chirib yuborish
-console.log(Object.keys(theif).length); // obyektimizning kalit so'zlarini massivga olib beradi
+console.log(hair);
+console.log(style);
 
+// // console.log(theif.height); // 1.7
+
+// // Metodlar:
+
+// // delete theif.jacket; = o'chirish metodi - object dan biror bir narsa o'chirib yuborish
+// // console.log(Object.keys(theif).length); = obyektimizning kalit so'zlarini massivga olib beradi
+
+// // let count = 0;
+
+// // for(let key in theif) {
+
+//   // count++;
+
+//   // if(typeof theif[key] === "object") {
+//   //   for(let i in theif[key]) {
+//   //     console.log(`Property ${i} has value ${theif[key][i]}`);
+//   //   }
+//   // }else {
+//   //   console.log(`Property ${key} has value ${theif[key]}`);
+//   // }
+// }
+
+// console.log(count);
