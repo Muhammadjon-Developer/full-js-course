3125ca28c2608c3df2290e51d22f6305951a0b07
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-02 11:54:32 +0300

    lesson 21 | Dynamic typing

---
+++ Javascript Basics/lesson 21/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// to string - string data type ga o'tiqazish
+/* // 1-)
+console.log(typeof String(21)); // string */
+
+/* // 2-) 
+console.log(typeof ("Oybek " + 14)); // string  
+const youtubeChannel = 10;
+console.log("youtube.com/channel/" + youtubeChannel);*/
+
+// to number - number data type ga o'tishqazish
+/* // 1-)
+console.log(typeof Number("7")); // number */
+
+/* // 2-)
+console.log(typeof +9); // number */
+
+/* // 3-)
+console.log(typeof (parseInt("7"))); */
+
+// to boolean - boolean data type ga o'tishqazish
+
+// ESLAB QOLING: 0, "", null, undefined, NaN = xar doim false qaytaradi
+
+let age = 0;
+
+if (age) {
+  console.log("0");
+} else {
+  console.log("Error"); // bu console ishlaydi chunki 0 bizga false qaytaradi har doim
+}
+
+age = 18;
+
+if (age) {
+  console.log("18"); // bu console ishlaydi chunki biz 0 ni 18 ga o'zgartirdik
+} else {
+  console.log("Error");
+}
+
+console.log(typeof Boolean("5"))
+
+console.log(typeof !!"5");
+
