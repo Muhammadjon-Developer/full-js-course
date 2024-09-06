6e286c1d11c850c976eab649d97db046b311ae0f
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-06 16:29:27 +0300

    lesson 1 | classList | MODUL 2

---
+++ Javascript Projects/lesson 1/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const btns = document.querySelectorAll('button')
+
+// console.log(btns[0].classList.length) //classlar length ni anirlash
+// console.log(btns[0].classList.item(0)) // blue - classlarni index si yordamida ko'rsatish
+
+// btns[1].classList.add('blue') // class qo'shish
+// btns.[1].classList.add('blue', 'red') // 2 ta class qo'shish
+// console.log(btns[1])
+// btns[0].classList.remove('blue') // class o'chirish
+// btns[0].classList.toggle('blue') // shunday nomli class bo'lsa o'chirib tashlaydi, bo'lmasa qo'shadi
+
+// btns[0].classList.add('red')
+
+// if(btns[0].classList.contains('red')) { // contains o'z ichiga qamrab olsa yani biron-bir class element ichida bo'lsa bizning kodimizni alamga oshiradi
+//   console.log('yeap')
+// }
+
+btns[0].addEventListener('click', () => {
+  // if (!btns[1].classList.contains('red')) {
+  //   btns[1].classList.add('red')
+  // } else {
+  //   btns[1].classList.remove('red')
+  // }
+  btns[1].classList.toggle('red')
+})
