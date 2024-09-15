f7671660018b0bd73c5591e0cbac4cc34d7bc605
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-15 13:17:51 +0300

    leson 9 | Modal oyna loyiha

---
+++ Javascript Projects/lesson 9/js/app.js
@@ -0,0 +1,141 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabsParent = document.querySelector(".tabheader__items"),
+    tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    loader = document.querySelector(".loader");
+
+  // Loader
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 2000);
+
+  // Tabs
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, idx) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(idx);
+        }
+      });
+    }
+  });
+
+  // Timer
+  const deadLine = "2024-12-30";
+
+  // console.log(Date.parse("2024-12-30")); // shu data (vaqt) gacha qolgan vaqt millisekundlarda
+
+  function getTimeReamaining(endTime) {
+    let days, hours, minutes, seconds;
+    const timer = Date.parse(endTime) - Date.parse(new Date()); // biz bergan vaqtdan hozirgi vaqtni ayirish
+
+    if (timer <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(timer / (1000 * 60 * 60 * 24)); // = kun,
+      hours = Math.floor((timer / (1000 * 60 * 60)) % 24); // = soat;
+      minutes = Math.floor((timer / (1000 / 60)) % 60); // = minut
+      seconds = Math.floor((timer / 1000) % 60); // = sekund
+    }
+    // return { timer: timer, days: days, hours: hours, minutes: minutes, seconds:seconds }; // eski usul
+    return { timer, days, hours, minutes, seconds }; // ES8 yangi usul o'zgaruvchi nomlari bilan bir-xil bo'lishi kerak
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeReamaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.timer <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadLine);
+
+  // Modal
+  const modalTrigger = document.querySelector("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
+  modalTrigger.addEventListener("click", openModal);
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+});
