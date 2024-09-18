091ce6f25284dbead877f9d2dbb1b1bc1d5124dc
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-18 16:39:44 +0300

    lesson 11 | function constructor

--- Javascript Projects/lesson 11/app.js
+++ Javascript Projects/lesson 11/app.js
@@ -13,12 +13,22 @@ function Car(name, color, kph) {
   this.isAirBag = true;
   this.kph = kph;
   this.speed = () => {
-    console.log(`Speed of car ${this.name} is ${this.kph}`);
+    console.log(`Speed of car ${this.name} is ${this.kph}`); // property
   };
 }
 
-const mercedes = new Car("Mercedes_cls63", "Black", 250); // funksiyani constructor qilib ishlatish
-const bmw = new Car("Bmw_M5_Competition", "Gray", 200); // funksiyani constructor qilib ishlatish
+Car.prototype.sayHello = function () {
+  console.log(`Hello ${this.name} !`); // method
+};
+
+const mercedes = new Car("Mercedes_cls63", "Black", 300); // funksiyani constructor qilib ishlatish
+const bmw = new Car("Bmw_M5_Competition", "Gray", 305); // funksiyani constructor qilib ishlatish
+
+mercedes.sayHello();
+bmw.sayHello();
+
+mercedes.speed();
+bmw.speed();
 
 console.log(mercedes);
 console.log(bmw);
